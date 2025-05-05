d8b0fbeae4dc8f2960da5ee106aa22dc94b6496e
mrazpiazu <fgazpiazu@gmail.com>
2025-05-05 12:57:06 +0200

    fixed agg stock bars

--- utils/database/agg_stock_bars.sql
+++ utils/database/agg_stock_bars.sql
@@ -1,21 +1,21 @@
 with raw_bars as (
-    select
-        *
+    select *
     from fact_stock_bars
     where created_at between :start_time and :end_time
 )
-insert into agg_stock_bars
-    (created_at, symbol, open, close, high, low, volume, number_trades, volume_weighted_average_price, aggregation)
+insert into agg_stock_bars (
+    created_at, symbol, open, close, high, low, volume, number_trades, volume_weighted_average_price, aggregation
+)
 select
     :end_time as created_at,
     symbol,
-    median(open) as open,
-    median(close) as close,
-    median(high) as high,
-    median(low) as low,
-    median(volume) as volume,
-    median(number_trades) as number_trades,
-    median(volume_weighted_average_price) as volume_weighted_average_price,
+    percentile_cont(0.5) within group (order by open) as open,
+    percentile_cont(0.5) within group (order by close) as close,
+    percentile_cont(0.5) within group (order by high) as high,
+    percentile_cont(0.5) within group (order by low) as low,
+    percentile_cont(0.5) within group (order by volume) as volume,
+    percentile_cont(0.5) within group (order by number_trades) as number_trades,
+    percentile_cont(0.5) within group (order by volume_weighted_average_price) as volume_weighted_average_price,
     :aggregation
 from raw_bars
-group by created_at, symbol;
+group by symbol;
