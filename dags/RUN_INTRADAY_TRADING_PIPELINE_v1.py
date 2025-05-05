7112137f8d5d74aba6353041908040312517a3cd
mrazpiazu <fgazpiazu@gmail.com>
2025-05-05 13:48:01 +0200

    fixed intervals

--- dags/RUN_INTRADAY_TRADING_PIPELINE_v1.py
+++ dags/RUN_INTRADAY_TRADING_PIPELINE_v1.py
@@ -15,7 +15,7 @@ get_logger_config(logging)
     dag_id="intraday_trading_pipeline_dag_v1",
     start_date=datetime.datetime(2025, 5, 5, 12),
     schedule='*/15 12-19 * * *',
-    catchup=False,
+    catchup=True,
     max_active_runs=1,
     default_args={
         'owner': 'airflow',
