8d83f6e98885478aeb319d9e7229846b6ecb3a4f
mrazpiazu <fgazpiazu@gmail.com>
2025-05-05 16:10:55 +0200

    fixed

--- utils/database/agg_stock_bars.sql
+++ utils/database/agg_stock_bars.sql
@@ -9,13 +9,13 @@ insert into agg_stock_bars (
 select
     :end_time as created_at,
     symbol,
-    round(percentile_cont(0.5) within group (order by open), 4) as open,
-    round(percentile_cont(0.5) within group (order by close), 4) as close,
-    round(percentile_cont(0.5) within group (order by high), 4) as high,
-    round(percentile_cont(0.5) within group (order by low), 4) as low,
-    round(percentile_cont(0.5) within group (order by volume), 4) as volume,
-    round(percentile_cont(0.5) within group (order by number_trades), 4) as number_trades,
-    round(percentile_cont(0.5) within group (order by volume_weighted_average_price), 4) as volume_weighted_average_price,
+    round(percentile_cont(0.5) within group (order by open)::NUMERIC, 4) as open,
+    round(percentile_cont(0.5) within group (order by close)::NUMERIC, 4) as close,
+    round(percentile_cont(0.5) within group (order by high)::NUMERIC, 4) as high,
+    round(percentile_cont(0.5) within group (order by low)::NUMERIC, 4) as low,
+    round(percentile_cont(0.5) within group (order by volume)::NUMERIC, 4) as volume,
+    round(percentile_cont(0.5) within group (order by number_trades)::NUMERIC, 4) as number_trades,
+    round(percentile_cont(0.5) within group (order by volume_weighted_average_price)::NUMERIC, 4) as volume_weighted_average_price,
     :aggregation
 from raw_bars
 group by symbol;
