86b0271dbf6571c6d602abd10a99e8ff67693b77
mrazpiazu <fgazpiazu@gmail.com>
2025-05-08 16:57:50 +0200

    fixed

--- utils/database/agg_stock_bars_candles.sql
+++ utils/database/agg_stock_bars_candles.sql
@@ -21,22 +21,33 @@ joined AS (
     ON f.created_at >= i.bucket_start
    AND f.created_at < i.bucket_start + INTERVAL :aggregation
 ),
+ordered AS (
+  SELECT
+    *,
+    FIRST_VALUE(open) OVER w AS first_open,
+    LAST_VALUE(close) OVER w AS last_close
+  FROM joined
+  WINDOW w AS (
+    PARTITION BY symbol, bucket_start ORDER BY created_at
+    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+  )
+),
 resampled AS (
   SELECT
     bucket_start AS created_at,
     symbol,
-    FIRST_VALUE(open) OVER w AS open,
-    LAST_VALUE(close) OVER w AS close,
+    MIN(first_open) AS open,
+    MIN(last_close) AS close,
     MAX(high) AS high,
     MIN(low) AS low,
     SUM(volume) AS volume,
     SUM(number_trades) AS number_trades,
     SUM(volume * close)::NUMERIC / NULLIF(SUM(volume), 0) AS volume_weighted_average_price,
     :aggregation AS aggregation
-  FROM joined
-  WINDOW w AS (
-    PARTITION BY symbol, bucket_start ORDER BY created_at
-    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-  )
+  FROM ordered
   GROUP BY bucket_start, symbol
 )
+INSERT INTO agg_stock_bars (
+  created_at, symbol, open, close, high, low, volume, number_trades, volume_weighted_average_price, aggregation
+)
+SELECT * FROM resampled;
