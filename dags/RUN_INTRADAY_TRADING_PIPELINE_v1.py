f9f6d7cb9007077510f0dfc310a03d618ab3e11e
mrazpiazu <fgazpiazu@gmail.com>
2025-05-09 19:24:28 +0200

    fixed

--- dags/RUN_INTRADAY_TRADING_PIPELINE_v1.py
+++ dags/RUN_INTRADAY_TRADING_PIPELINE_v1.py
@@ -3,8 +3,8 @@ from airflow.decorators import dag, task, task_group
 from airflow.operators.python import get_current_context
 import logging
 
-from utils.database.functions import *
-from utils.technical_analysis.functions import *
+from utils.database.functions import run_backfill_fact_stock_bars_api, run_backfill_fact_stock_bars, run_agg_stock_bars_candles
+from utils.technical_analysis.functions import run_technical_analysis_sql
 from utils.logger.logger import get_logger_config
 
 logger = logging.getLogger("intraday_trading_pipeline_dag")
