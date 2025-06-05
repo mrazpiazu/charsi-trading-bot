f65dcafebd457a1431e18f2fd580ba9a89da7dee
mrazpiazu <fgazpiazu@gmail.com>
2025-06-05 09:46:11 +0200

    fixed bracket

--- utils/brokers/alpaca_market/alpaca_transactions.py
+++ utils/brokers/alpaca_market/alpaca_transactions.py
@@ -5,8 +5,8 @@ import logging
 import os
 from dotenv import load_dotenv
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import MarketOrderRequest, TakeProfitRequest, StopLossRequest
-from alpaca.trading.enums import OrderSide, OrderType, TimeInForce
+from alpaca.trading.requests import MarketOrderRequest, TakeProfitRequest, StopLossRequest, StopLimitOrderRequest, LimitOrderRequest, StopOrderRequest
+from alpaca.trading.enums import OrderSide, OrderType, TimeInForce, OrderClass
 
 from utils.technical_analysis.indicators import *
 from utils.database.db import *
@@ -18,7 +18,7 @@ get_logger_config(logging)
 load_dotenv()
 
 
-def place_order(symbol, qty, take_profit, stop_loss):
+def place_order(symbol, qty, take_profit, stop_loss, paper=True):
     """
     Place an order with Alpaca API.
     """
@@ -27,7 +27,7 @@ def place_order(symbol, qty, take_profit, stop_loss):
     client = TradingClient(
         api_key=os.getenv("ALPACA_API_KEY_ID"),
         secret_key=os.getenv("ALPACA_API_SECRET_KEY"),
-        paper=True
+        paper=paper
     )
 
     take_profit_request = TakeProfitRequest(
@@ -40,19 +40,20 @@ def place_order(symbol, qty, take_profit, stop_loss):
 
     # preparing orders
     market_order_data = MarketOrderRequest(
-                        symbol=symbol,
-                        qty=qty,
-                        side=OrderSide.BUY,
-                        time_in_force=TimeInForce.DAY,
-                        order_type=OrderType.MARKET,
-                        take_profit=take_profit_request,
-                        stop_loss=stop_loss_request
-                        )
+        symbol=symbol,
+        qty=qty,
+        side=OrderSide.BUY,
+        time_in_force=TimeInForce.DAY,
+        order_class=OrderClass.BRACKET,
+        order_type=OrderType.MARKET,
+        take_profit=take_profit_request,
+        stop_loss=stop_loss_request
+    )
 
     # Market order
     market_order = client.submit_order(
-                    order_data=market_order_data
-                   )
+        order_data=market_order_data
+    )
 
     return
 
@@ -77,6 +78,8 @@ if __name__ == "__main__":
     from utils.llm.xynth_finance.xynth_playwright import run_xynth_consultation_pipeline
     import asyncio
 
-    trading_actions = asyncio.run(run_xynth_consultation_pipeline())
+    # trading_actions = asyncio.run(run_xynth_consultation_pipeline())
+
+    trading_actions = [{'entry_point': 118.96, 'expected_duration': '12 days', 'position_size': 4, 'potential_profit_loss': 88.18, 'risk_reward_ratio': 2.5, 'stock': 'AMD', 'stop_loss': 110.14, 'target_price': 141.01}, {'entry_point': 83.84, 'expected_duration': '6 days', 'position_size': 3, 'potential_profit_loss': 25.1, 'risk_reward_ratio': 1.78, 'stock': 'UBER', 'stop_loss': 79.15, 'target_price': 92.21}, {'entry_point': 137.97, 'expected_duration': '6 days', 'position_size': 1, 'potential_profit_loss': 12.6, 'risk_reward_ratio': 2.0, 'stock': 'NVDA', 'stop_loss': 131.67, 'target_price': 150.58}]
 
     run_place_order_pipeline(trading_actions)
